c797c6fdd07b9006bca78795584f65a425d477bd
John Kilima <jkbundala@gmail.com>
2024-09-06 14:20:31 +0900

    Servlet Project: Client Side redirection

--- servlet-demo/src/main/java/dev/kilima/servlet/TableServlet.java
+++ servlet-demo/src/main/java/dev/kilima/servlet/TableServlet.java
@@ -16,14 +16,26 @@ public class TableServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String input = request.getParameter("num");
-		int num = Integer.parseInt(input);
+		
+		if(input==null) {
+			response.sendRedirect("table.html");
+			return;
+		}
+		
+		
 
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		
-		out.println("<h3>Table for " + num + ": </h3>");
-		for (int i = 1; i <= 10; i++) {
-			out.printf("%d X %d = %d <br>", num, i, num * i);
+		try {
+			int num = Integer.parseInt(input);
+			out.println("<h3>Table for " + num + ": </h3>");
+			for (int i = 1; i <= 10; i++) {
+				out.printf("%d X %d = %d <br>", num, i, num * i);
+			}
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			out.println("<h3>Invalid input. Only intergers allowed!</h3>");
+			e.printStackTrace();
 		}
 		out.close();
 	}
