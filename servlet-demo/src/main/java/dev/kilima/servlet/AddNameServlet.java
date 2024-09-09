157addaa7673cc53219706a3e2b2032c14d0e462
John Kilima <jkbundala@gmail.com>
2024-09-09 18:00:52 +0900

    Servlet Project: Sesion Scope part 1

--- servlet-demo/src/main/java/dev/kilima/servlet/AddNameServlet.java
+++ servlet-demo/src/main/java/dev/kilima/servlet/AddNameServlet.java
@@ -1,7 +1,39 @@
 package dev.kilima.servlet;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/add-name")
 public class AddNameServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String friendName = req.getParameter("friend_name");
+		
+		if(friendName==null || friendName.trim().length()==0) {
+			resp.sendRedirect("./add-name.html");
+			return;
+		}
+		HttpSession session = req.getSession();
+		List<String> names = (List<String>) session.getAttribute("nameList");
+		if(names==null) {
+			names = new ArrayList<String>();
+			names.add(friendName);
+			session.setAttribute("nameList", names);
+		}
+	}
 
 }
