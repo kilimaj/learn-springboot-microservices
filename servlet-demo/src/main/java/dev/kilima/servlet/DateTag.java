190a776946c5544ec96b5b017e79bb139dd8bae2
John Kilima <jkbundala@gmail.com>
2024-09-19 17:59:51 +0900

    JSP Project: Creating a JSP custom action

---
+++ servlet-demo/src/main/java/dev/kilima/servlet/DateTag.java
@@ -0,0 +1,29 @@
+package dev.kilima.servlet;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class DateTag extends TagSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public int doStartTag() throws JspException {
+	
+		JspWriter out = pageContext.getOut();
+		try {
+			out.println(new Date().toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return super.doStartTag();
+	}
+	
+	
+
+}
---
+++ servlet-demo/src/main/webapp/custom-tags-demo.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Custom Tags Demo</title>
+</head>
+<body>
+<h1>Custom Tags Demo</h1>
+<hr>
+<date />
+</body>
+</html>
