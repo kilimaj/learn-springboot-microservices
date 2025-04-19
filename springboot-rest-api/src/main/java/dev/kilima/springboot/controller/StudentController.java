568b12e46da8412a9dda4d998a1e2371315636af
John Kilima <jkbundala@gmail.com>
2025-04-19 05:28:36 +0300

    Create Simple Springboot Rest API That returns Java Bean

---
+++ springboot-rest-api/src/main/java/dev/kilima/springboot/controller/StudentController.java
@@ -0,0 +1,19 @@
+package dev.kilima.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.kilima.springboot.bean.Student;
+
+@RestController
+public class StudentController {
+
+	// http://localhost:8080/student
+	@GetMapping("/student")
+	public Student getStudent() {
+		Student student = new Student(1, "John", "Kilima");
+		return student;
+	}
+
+
+}
