23d2dde8332f1554fd282da171421afd1655901f
John Kilima <jkbundala@gmail.com>
2025-04-19 05:34:28 +0300

    Create Simple Springboot Rest API That returns List

--- springboot-rest-api/src/main/java/dev/kilima/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/dev/kilima/springboot/controller/StudentController.java
@@ -1,5 +1,8 @@
 package dev.kilima.springboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +18,14 @@ public class StudentController {
 		return student;
 	}
 
-
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		List<Student> students = new ArrayList<>();
+		students.add(new Student(1, "John", "Kilima"));
+		students.add(new Student(2, "Mabula", "Kilima"));
+		students.add(new Student(3, "Barnabas", "Kilima"));
+		students.add(new Student(4, "Gabriel", "Kilima"));
+   
+		return students;
+	}
 }
