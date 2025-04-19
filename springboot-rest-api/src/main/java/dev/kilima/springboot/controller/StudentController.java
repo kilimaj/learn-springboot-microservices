e4c619769b0eed706a7f73a4c6800f73279eb759
John Kilima <jkbundala@gmail.com>
2025-04-19 05:54:00 +0300

    Create Simple Springboot Rest API with Path Variable

--- springboot-rest-api/src/main/java/dev/kilima/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/dev/kilima/springboot/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.kilima.springboot.bean.Student;
@@ -18,6 +19,7 @@ public class StudentController {
 		return student;
 	}
 
+	// http://localhost:8080/students
 	@GetMapping("/students")
 	public List<Student> getStudents() {
 		List<Student> students = new ArrayList<>();
@@ -27,5 +29,28 @@ public class StudentController {
 		students.add(new Student(4, "Gabriel", "Kilima"));
    
 		return students;
+	}
+
+	// Springboot Rest API with path variable
+	// {id} - URI template variable
+	// http://localhost:8080/student-byid/1
+	@GetMapping("student-byid/{id}")
+	public Student studentPathVariable(@PathVariable int id) {
+		return new Student(id, "John", "Kilima");
+	}
+
+	// When the URI template variable name is different
+	// http://localhost:8080/student-byname/John
+	@GetMapping("student-byname/{name}")
+	public Student studentNewPathVariable(@PathVariable("name") String firstName) {
+		return new Student(1, firstName, "Kilima");
+	}
+
+	// Can also accommodate multiple variables
+	// http://localhost:8080/student-full/1/John/Kilima
+	@GetMapping("student-full/{id}/{firstName}/{lastName}")
+	public Student studentNewPathVariable(@PathVariable("id") int studentId, @PathVariable String firstName,
+			@PathVariable String lastName) {
+		return new Student(studentId, firstName, lastName);
 	}
 }
