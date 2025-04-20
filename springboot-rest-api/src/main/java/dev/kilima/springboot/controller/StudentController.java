be41dd1eca75a370fdad5e942334f76fd26b5b1e
John Kilima <jkbundala@gmail.com>
2025-04-20 12:39:54 +0300

    Spring boot REST API handles HTTP POST Request

--- springboot-rest-api/src/main/java/dev/kilima/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/dev/kilima/springboot/controller/StudentController.java
@@ -3,9 +3,13 @@ package dev.kilima.springboot.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.kilima.springboot.bean.Student;
@@ -62,5 +66,17 @@ public class StudentController {
 	public Student studentRequestVariable(@RequestParam int id, @RequestParam String firstName,
 			@RequestParam String lastName) {
 		return new Student(id, firstName, lastName);
+	}
+
+	// Spring boot REST API handles HTTP POST Request
+	// @PostMapping and @RequestBody This is used to retrieve json from http request
+	// format and convert to java object
+	@PostMapping("students/create")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Student createStudent(@RequestBody Student student) {
+		System.out.println(student.getId());
+		System.out.println(student.getFirstName());
+		System.out.println(student.getLastName());
+		return student;
 	}
 }
