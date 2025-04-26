7385f72c2864157d51e2cd2cf519c2531dc83deb
John Kilima <jkbundala@gmail.com>
2025-04-26 16:51:19 +0300

    SpringBoot REST API that handles HTTP PUT Request - updating existing

--- springboot-rest-api/src/main/java/dev/kilima/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/dev/kilima/springboot/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -75,6 +76,15 @@ public class StudentController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Student createStudent(@RequestBody Student student) {
 		System.out.println(student.getId());
+		System.out.println(student.getFirstName());
+		System.out.println(student.getLastName());
+		return student;
+	}
+
+	// SpringBoot REST API that handles HTTP PUT Request - updating existing
+	// resource
+	@PutMapping("students/{id}/update")
+	public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
 		System.out.println(student.getFirstName());
 		System.out.println(student.getLastName());
 		return student;
