4be04373bd3a3e3f2c28b8f1246353ac12c4a0df
John Kilima <jkbundala@gmail.com>
2025-05-01 16:25:36 +0300

    Spring Boot REST API that handles HTTP DELETE Request - deleting the existing resource

--- springboot-rest-api/src/main/java/dev/kilima/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/dev/kilima/springboot/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -89,4 +90,13 @@ public class StudentController {
 		System.out.println(student.getLastName());
 		return student;
 	}
+
+	// Spring Boot REST API that handles HTTP DELETE Request - deleting the existing
+	// resource
+	@DeleteMapping("students/{id}/delete")
+	public String deleteStudent(@PathVariable("id") int studentId) {
+		System.out.println(studentId);
+		return "Student deleted successfully!";
+	}
+
 }
